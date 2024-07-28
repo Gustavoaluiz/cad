56872d8f92c0a0d85ec5f1153cc57fb4476b827e
Gustavoaluiz <gustavoaluizbp@gmail.com>
2024-07-28 01:04:31 -0300

    muda para grasp normal

--- main.c
+++ main.c
@@ -6,17 +6,10 @@
 #define N 228
 // #define N 20
 
-typedef struct {
-    double* alphas;  // Lista com os alphas possíveis de serem escolhidos
-    double* scores;  // Quantas vezes cada alpha foi usado ao longo das iterações
-    int* usos;  // Quantidade de amostras de cada valor de alphas para começar a atualizar as probabilidades
-    int num_amostras;
-    int qtd;  // Quantidade de alphas possíveis
-} Alphas;
 
 typedef struct {
     float** matriz_custo;  // Matriz com os custos de ir de uma cidade para outra
-    Alphas alpha;
+    float alpha;
     int max_iter;
     int cidade_partida;
     int* melhor_solucao;
@@ -49,66 +42,16 @@ float** ler_matriz_custo(const char* nome_arquivo, int tamanho_matriz) {
     return matriz;
 }
 
-void inicializa_alphas(Alphas* alpha, double* alphas, int qtd, int num_amostras) {
-    alpha->alphas = alphas;
-    alpha->scores = (double*)calloc(qtd, sizeof(double));
-    alpha->usos = (int*)calloc(qtd, sizeof(int));
-    alpha->num_amostras = num_amostras;
-    alpha->qtd = qtd;
-}
-
-void inicializa_grasp(GraspReativo* grasp, float** matriz, double* alphas, int qtd, int num_amostras, int max_iter, int cidade_partida) {
+void inicializa_grasp(GraspReativo* grasp, float** matriz, float alpha, int max_iter, int cidade_partida) {
     grasp->matriz_custo = matriz;
-    inicializa_alphas(&grasp->alpha, alphas, qtd, num_amostras);
+    grasp->alpha = alpha;
     grasp->max_iter = max_iter;
     grasp->cidade_partida = cidade_partida;
     grasp->melhor_solucao = NULL;
     grasp->melhor_custo = INF;
 }
 
-void atualizar_alpha_prob(GraspReativo* grasp, int index, float custo_solucao) {
-    /*
-    Atualiza o score do alpha com o inverso do custo da solução,
-    ou seja, um custo menor gera um score maior
-    */
-    grasp->alpha.scores[index] += 1.0 / (custo_solucao + 1);
-}
-
-int retorna_alpha(GraspReativo* grasp, int iteracao) {
-    int index;
-    
-    if (iteracao >= grasp->alpha.num_amostras * grasp->alpha.qtd) {
-        double* prob_alphas = (double*)malloc(grasp->alpha.qtd * sizeof(double));
-        double soma_scores = 0;
-
-        for (int i = 0; i < grasp->alpha.qtd; i++) {
-            prob_alphas[i] = grasp->alpha.scores[i] / grasp->alpha.usos[i];
-            soma_scores += prob_alphas[i];
-        }
-
-        for (int i = 0; i < grasp->alpha.qtd; i++) {
-            prob_alphas[i] /= soma_scores;
-        }
-
-        double random_value = (double)rand() / RAND_MAX;
-        double cumulative_probability = 0.0;
-        for (index = 0; index < grasp->alpha.qtd; index++) {
-            cumulative_probability += prob_alphas[index];
-            if (random_value <= cumulative_probability) {
-                break;
-            }
-        }
-
-        grasp->alpha.usos[index]++;
-        free(prob_alphas);
-    } else {
-        index = iteracao % grasp->alpha.qtd;
-        grasp->alpha.usos[index]++;
-    }
-    return index;
-}
-
-int* construcao(GraspReativo* grasp, double alpha, int tamanho_matriz) {
+int* construcao(GraspReativo* grasp, int tamanho_matriz) {
     int indice_atual = grasp->cidade_partida;
     int* solucao = (int*)malloc(tamanho_matriz * sizeof(int));
     int* restantes = (int*)malloc(tamanho_matriz * sizeof(int));
@@ -137,7 +80,7 @@ int* construcao(GraspReativo* grasp, double alpha, int tamanho_matriz) {
             if (lista_candidatos[i] > max) max = lista_candidatos[i];
         }
 
-        int limite = min + alpha * (max - min);
+        int limite = min + grasp->alpha * (max - min);
         int* rcl = (int*)malloc(tam_restantes * sizeof(int));
         int rcl_size = 0;
 
@@ -220,10 +163,8 @@ void busca_local(int* solucao, GraspReativo* grasp, int tamanho_matriz, int max_
 void gerar_solucao(GraspReativo* grasp, int tamanho_matriz, int max_iter_local) {
     for (int iteracao = 0; iteracao < grasp->max_iter; iteracao++) {
         printf("Iteração graspzada %d\n", iteracao);
-        int alpha_index = retorna_alpha(grasp, iteracao);
-        double alpha = grasp->alpha.alphas[alpha_index];
 
-        int* solucao_inicial = construcao(grasp, alpha, tamanho_matriz);
+        int* solucao_inicial = construcao(grasp, tamanho_matriz);
 
         busca_local(solucao_inicial, grasp, tamanho_matriz, max_iter_local);
         float custo_solucao = calcula_custo(grasp, solucao_inicial, tamanho_matriz);
@@ -237,8 +178,6 @@ void gerar_solucao(GraspReativo* grasp, int tamanho_matriz, int max_iter_local) 
         } else {
             free(solucao_inicial);
         }
-
-        atualizar_alpha_prob(grasp, alpha_index, custo_solucao);
     }
 }
 
@@ -255,15 +194,13 @@ int main() {
     // Converte a matriz estática para um ponteiro de ponteiros
     float** matriz = ler_matriz_custo(nome_arquivo, tamanho_matriz);
 
-    double alphas[] = {0.2, 0.25, 0.3, 0.35, 0.4};
-    int qtd_alphas = sizeof(alphas) / sizeof(alphas[0]);
-    int num_amostras = 10;
+    float alpha = 0.2;
     int max_grasp_iter = 50;
     int max_iter_local = 1000;
     int cidade_partida = 0;
 
     GraspReativo grasp;
-    inicializa_grasp(&grasp, matriz, alphas, qtd_alphas, num_amostras, max_grasp_iter, cidade_partida);
+    inicializa_grasp(&grasp, matriz, alpha, max_grasp_iter, cidade_partida);
 
     gerar_solucao(&grasp, tamanho_matriz, max_iter_local);
 
@@ -278,8 +215,6 @@ int main() {
     }
     free(matriz);
     free(grasp.melhor_solucao);
-    free(grasp.alpha.scores);
-    free(grasp.alpha.usos);
 
     return 0;
 }
